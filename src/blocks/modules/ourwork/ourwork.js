9189dbc138dd72a2d723e319381aeadf432b1b3a
Nikita <nikita119@protonmail.com>
2022-10-18 14:40:41 +0300

    agent-section

---
+++ src/blocks/modules/ourwork/ourwork.js
@@ -0,0 +1,13 @@
+modules.define('ourwork', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
