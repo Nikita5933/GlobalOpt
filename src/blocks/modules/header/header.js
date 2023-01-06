6380c52606e9338caa5c8d5d70ba4af8355aab15
Nikita <100522235+Nikita5933@users.noreply.github.com>
2023-01-06 22:32:53 +0300

    mobile4

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -2,12 +2,16 @@
 
 const hamburger = document.querySelector('.hamburger'),
       aside = document.querySelector('.promo__wrapper-aside'),
-      close = document.querySelector('[data-close]');
+      close = document.querySelector('[data-close]'),
+      body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
     aside.classList.toggle('active');
+    body.style.overflow = 'hidden';
 });
 
 close.addEventListener('click', () => {
     aside.classList.toggle('active');
+    body.style.overflow = 'scroll';
+
 });
