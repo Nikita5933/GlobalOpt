a7e493f1197200387098f691fa817e19c9f6560e
Nikita <nikita119@protonmail.com>
2024-01-02 17:00:03 +0300

    gh-pages

--- src/blocks/modules/map/map.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const btnClose = document.querySelectorAll('[data-close]'),
-      mapWindow = document.querySelector('.map__window');
-
-btnClose[1].addEventListener('click', () => {
-    mapWindow.style.display = 'none';
-});
--- src/js/import/modules.js
+++
@@ -1,4 +0,0 @@
-import "%modules%/header/header";
-import "%modules%/footer/footer";
-import "%modules%/reviews/reviews";
-import "%modules%/map/map";
